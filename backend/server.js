3522d8a8f5c9a67f47ae5401e04043b8bddc8d00
Jihad <ji.0rabiii@gmail.com>
2025-01-26 23:52:14 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/user.js"
 import postRoutes from "./routes/post.js"
 import notificationRoutes from "./routes/notification.js"
 import dotenv from "dotenv"
+import path from "path"
 import { connectDB } from "./db/connectDB.js"
 import cookieParser from "cookie-parser"
 import {v2 as cloudinary} from "cloudinary"
@@ -13,6 +14,8 @@ cloudinary.config({
     api_key:process.env.CLOUDINARY_API_KEY ,
     api_secret:process.env.CLOUDINARY_API_SECRET
 })
+
+const __dirname = path.resolve()
 const app = express()
 app.use(express.json({limit:"5mb"}))
 app.use(express.urlencoded({extended:true})) //postman form-urlencoded
@@ -21,6 +24,14 @@ app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/notifications",notificationRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(5000,()=>{
     console.log("Server running")
