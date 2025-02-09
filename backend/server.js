ddf9c712839e5391bfc2b6fe3eb8cc52ce444744
Jihad <ji.0rabiii@gmail.com>
2025-02-09 22:43:09 +0200

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,12 @@ app.use("/api/posts",postRoutes)
 app.use("/api/notifications",notificationRoutes)
 app.use("/api/chats",chatRoutes)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+
 server.listen(port,()=>{
     console.log("Server running")
     connectDB()
