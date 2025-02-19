803de4451531c53f8a749a3a3e8791e03a9d0d6b
Jihad <ji.0rabiii@gmail.com>
2025-02-19 21:19:08 +0200

    update

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -1,6 +1,6 @@
  import jwt from "jsonwebtoken"
 export const generateTokensAndSetCookie = (userId,res)=>{
-   const token = jwt.sign({userId},process.env.JWT_SECRET_KEY,{expiresIn:"15d"})
+   const token = jwt.sign({userId},process.env.JWT_SECRET_KEY,{expiresIn:"5h"})
    res.cookie("jwt",token,{
     maxAge: 15*24*60*60*1000,
     httpOnly:true,
